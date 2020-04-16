82ca671bb235c523f9b7dd347d753b4ccd25e48d
Machele <smithbeblack@gmail.com>
2020-04-16 10:54:19 +0000

    implemented field error tooltip - signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
-# from django.utils.safestring import mark_safe
+from django.utils.html import format_html, format_html_join
+from django.forms.utils import ErrorList
+
+#TODO: for custom form errors display
+class DivErrorList(ErrorList):
+    def __str__(self):
+        return self.as_divs()
+    def as_divs(self):
+        if not self: return ''
+        return '<div class="field-errorlist">{}</div>'.format(''.join(['<div class="error">%s</div>' %e for e in self]))
+
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -30,7 +40,8 @@ class SignupForm(UserCreationForm):
                         <span>Your password can't be entirely numeric.</span>
                     """
                     )    
-    
+
+        self.error_class = DivErrorList   #? implement custom form errors display
 
 class LoginForm(AuthenticationForm):
 
