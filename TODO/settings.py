32b6fddb18d21ab37da1bb9eb3f9c9410b2e0540
Machele <smithbeblack@gmail.com>
2020-05-05 13:03:41 +0000

    django-heroku

--- TODO/settings.py
+++ TODO/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,8 +124,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    ]   
+]   
 
 LOGIN_URL = '/'
 LOGIN_REDIRECT_URL = 'tasks:items'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
