973ac516d721f008c632c9b60a056449d5f1d1b7
Machele <smithbeblack@gmail.com>
2020-05-05 16:08:05 +0000

    db

--- TODO/settings.py
+++ TODO/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'TODO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASE_URL = os.environ['DATABASE_URL']
 import dj_database_url
